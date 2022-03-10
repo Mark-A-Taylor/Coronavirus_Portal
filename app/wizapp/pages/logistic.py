7177c2de70bce7b51d0b17629da410e6761cddb9
Frank <fcheng@illumina.com>
2022-03-10 12:33:36 -0800

    finalized

--- app/wizapp/pages/logistic.py
+++ app/wizapp/pages/logistic.py
@@ -3,9 +3,23 @@ from dash import html
 from dash.dependencies import Input, Output
 import pandas as pd 
 from app import app
-
+import plotly.express as px
 ## data 
 df_weight = pd.read_csv('data/bootstrap_features_selected.csv')
+
+
+## fig 
+fig = px.bar(
+    df_weight.sort_values('avg_weight',ascending = False),
+    x="avg_weight",
+    y='gene',
+    error_x= 'std_weight',
+    color = "avg_weight",
+    hover_data=["counts","std_weight"],
+    color_continuous_scale='prgn_r',
+    template='plotly_white'
+) 
+
 
 ## layout 
 layout = html.Div([
@@ -17,27 +31,30 @@ layout = html.Div([
     html.Table(
         id='bootstrap_stats_table', 
         className='bootstrap_stats_table', 
+        style = {"width":"100%"},
         children=[
-            html.Tr( [html.Th('Training Statistics'), html.Th("Gene Weight"), html.Th('Heatmap')] ),            
+            html.Tr( [
+                html.Th('Training Statistics',style = {"width":"30%"}), 
+                html.Th("Gene Weight",style = {"width":"30%"}), 
+                html.Th('Heatmap',style = {"width":"40%"})] ),            
             html.Tr(
                 children=[                       
                     html.Td(
                         html.Img(
-                            src = app.get_asset_url('fig7a_bootstrap.svg')
+                            src = app.get_asset_url('fig7a_bootstrap.svg'),
+                            style = {"width":"100%"}
                         )
                     ),
-                    html.Td(            
-                        dash_table.DataTable(
-                            data = df_weight.to_dict('records'), 
-                            columns = [{"name": i, "id": i} for i in df_weight.columns]
-                        )),                    
+                    html.Td(  
+                        html.Div(
+                            dcc.Graph(figure=fig)
+                        )          
+                    ),                    
                     html.Td(
-                        [html.Img(
-                            src = app.get_asset_url('fig7a.svg')
-                        ),
                         html.Img(
-                            src = app.get_asset_url('fig7b.svg')
-                        )]
+                            src = app.get_asset_url('fig7b.svg'),
+                            style = {"width":"100%"}
+                        )
                     )
                 ])
     ]),
